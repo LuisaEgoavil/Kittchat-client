8b1ccd8ac4440642dafa56834bfdffaefec28668
Megumikawa <megumi19901016@gmail.com>
2021-03-05 12:41:53 +0100

    add location slot

--- src/components/MyMap.js
+++ src/components/MyMap.js
@@ -4,71 +4,52 @@ import 'leaflet/dist/leaflet.css'
 import L from 'leaflet';
 
 class MyMap extends Component {
+
   render() {
-    const position1 = [51.505, -0.09]
-    const position2 = [51.605, -0.11]
-    const position3 = [51.705, -0.21]
-    const position4 = [51.805, -0.31]
-    const position5 = [51.905, -0.41]
+    const centerPosition = [52.517510, 13.393847]
+    const position1 = [52.503004, 13.429300]
+    const position2 = [52.518258, 13.305188]
+    const position3 = [52.494853, 13.428098]
+    const position4 = [52.474364, 13.433076]
+    const position5 = [52.535594, 13.424836]
 
-    const locationPin1 = new L.Icon({
+    const locationPin = new L.Icon({
       iconUrl: '../images/pin.png',
       iconSize: [60, 60]
-    })
-    const locationPin2 = new L.Icon({
-      iconUrl: '../images/pin.png',
-      iconSize: [60, 60]
-    })
-    const locationPin3 = new L.Icon({
-      iconUrl: '../images/pin.png',
-      iconSize: [60, 60]
-    })
-    const locationPin4 = new L.Icon({
-      iconUrl: '../images/pin.png',
-      iconSize: [60, 60]
-    })
-    const locationPin5 = new L.Icon({
-      iconUrl: '../images/pin.png',
-      iconSize: [60, 60]
-    })
-
-    const locationPin1 = new L.icon({
-      iconUrl: '../images/pin.png',
-      iconSize: [60,60]
     })
 
     return (
       <div>
-          <MapContainer style={{width: '600px', height: '400px'}}center={position1} zoom={13} scrollWheelZoom={false}>
+          <MapContainer style={{width: '700px', height: '500px'}}center={centerPosition} zoom={12} scrollWheelZoom={false}>
             <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
               
             <div className="location-spot">
-              <Marker icon={locationPin1} position={position1}>
+              <Marker icon={locationPin} position={position1}>
                 <Popup>Location1</Popup>
               </Marker>
             </div>
 
             <div className="location-spot">
-              <Marker icon={locationPin2} position={position2}>
+              <Marker icon={locationPin} position={position2}>
                 <Popup>Location2</Popup>
               </Marker>
             </div>
 
             <div className="location-spot">
-              <Marker icon={locationPin3} position={position3}>
+              <Marker icon={locationPin} position={position3}>
                 <Popup>Location3</Popup>
               </Marker>
             </div>
 
             <div className="location-spot">
-              <Marker icon={locationPin4} position={position4}>
+              <Marker icon={locationPin} position={position4}>
                 <Popup>Location4</Popup>
               </Marker>
             </div>
 
             <div className="location-spot">
-              <Marker icon={locationPin5} position={position5}>
+              <Marker icon={locationPin} position={position5}>
                 <Popup>Location5</Popup>
               </Marker>
             </div>
