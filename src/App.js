cbb2d4b4305148ac8f1212132bcb145180e7cf15
Megumikawa <megumi19901016@gmail.com>
2021-03-08 17:59:18 +0100

    create sample of cafe menu

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import MakeBooking from "./components/pages/MakeBooking";
 import MyMap from "./components/MyMap"
 import BookingList from "./components/pages/BookingList"
 import Header from './components/Header'
+import Footer from './components/Footer'
 import GoogleLogin from 'react-google-login'
 import './App.css';
 //-------------------------------------------------------------------//
@@ -163,6 +164,7 @@ componentDidMount(){
           }} />
           <MyMap />
         </Switch>
+        <Footer />
       </div>
     );
   }
--- src/components/pages/Cafe.js
+++ src/components/pages/Cafe.js
@@ -2,21 +2,133 @@ import React, { Component } from 'react'
 import Footer from '../Footer'
 
 class Cafe extends Component {
+
   render() {
     return (
       <div>
-        
-        <div className="main-body">
-          <div className="cafe-left">
-            <h2>Menu</h2>
-            <p>price: </p>
-          </div>
-          <div className="cafe-right">
-            <img src="" alt="cafemenu"/>
-            <p>names of the menu</p>
-          </div>
+        <div className="main-body cafe-body">
+          <h1>Menu</h1> 
+
+            <div className="cafe">
+                <div className="menu-wrap">
+                  <div className="menu-block">
+                    <h2>Coffee</h2>
+                    <span className="menu-detail">
+                      <p>Black Coffee</p>
+                      <p>€ 2.50</p>
+                    </span>
+                    <span className="menu-detail">
+                      <p>White Coffee</p>
+                      <p>€ 3.00</p>
+                    </span>
+                    <span className="menu-detail">
+                      <p>Cappuccino</p>
+                      <p>€ 3.50</p>
+                    </span>
+                    <span className="menu-detail">
+                      <p>Latte</p>
+                      <p>€ 3.50</p>
+                    </span>
+                    <span className="menu-detail">
+                      <p>Mocha</p>
+                      <p>€ 4.00</p>
+                    </span>
+                  </div>
+                  <img src="/images/cafe2.png" alt="cats" />
+                </div>
+
+                
+                <div className="menu-wrap">
+                  <div className="menu-block">
+                    <h2>Tea</h2>
+                    <span className="menu-detail">
+                    <p>Sweet Ginger Peach</p>
+                    <p>€ 3.00</p>
+                  </span>
+                    <span className="menu-detail">
+                    <p>HoneyBush Caramel</p>
+                    <p>€ 3.00</p>
+                  </span>
+                    <span className="menu-detail">
+                    <p>Peach Mango</p>
+                    <p>€ 3.00</p>
+                  </span>
+                  </div>
+                  <img src="/images/cafe2.png" alt="cats" />
+                </div>
+
+                
+                <div className="menu-wrap">
+                  <div className="menu-block">
+                    <h2>Others</h2>
+                    <span className="menu-detail">
+                      <p>Coke</p>
+                      <p>€ 2.50</p>
+                    </span>
+                    <span className="menu-detail">
+                      <p>Sprite</p>
+                      <p>€ 2.50</p>
+                    </span>
+                    <span className="menu-detail">
+                      <p>Mineral Water</p>
+                      <p>€ 2.00</p>
+                    </span>
+                    <span className="menu-detail">
+                      <p>Green Tea</p>
+                      <p>€ 2.50</p>
+                    </span>
+                    <span className="menu-detail">
+                      <p>Lemon Tea</p>
+                      <p>€ 2.50</p>
+                    </span>
+                    <span className="menu-detail">
+                      <p>Frizzy Lychee</p>
+                      <p>€ 4.00</p>
+                    </span>
+                    <span className="menu-detail">
+                  <p>Hot Chocolate</p>
+                  <p>€ 3.50</p>
+                </span>
+                  </div>
+                  <img src="/images/cafe2.png" alt="cats" />
+                </div>
+
+                
+                <div className="menu-wrap">
+                  <div className="menu-block">
+                    <h2>Desserts&Foods</h2>
+                    <span className="menu-detail">
+                      <p>Oreo Cheesecake</p>
+                      <p>€ 6.50</p>
+                    </span>
+                    <span className="menu-detail">
+                      <p>Matcha Azuki Cake</p>
+                      <p>€ 6.50</p>
+                    </span>
+                    <span className="menu-detail">
+                      <p>Raionbow Swiss Roll</p>
+                      <p>€ 6.50</p>
+                    </span>
+                    <span className="menu-detail">
+                      <p>Belgiam Waffles</p>
+                      <p>€ 4.00</p>
+                    </span>
+                    <span className="menu-detail">
+                      <p>Nachos with Salsa Dip</p>
+                      <p>€ 5.50</p>
+                    </span>
+                    <span className="menu-detail">
+                    <p>Curry and rice</p>
+                    <p>€ 7.50</p>
+                  </span>
+                  </div>
+                  <img src="/images/cafe2.png" alt="cats" />
+                </div>
+            </div>
+            
         </div>
-        <Footer />
+        
+        
       </div>
     )
   }
