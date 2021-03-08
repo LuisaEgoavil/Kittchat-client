3248e1b4d03b850283309e35485a247e45337719
Luisa Egoavil <lupi@Luisas-Air.fritz.box>
2021-03-08 18:02:03 +0100

    commit

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react'
-// import axios from 'axios'
-// import config from '../../config'
+
 
 class Profile extends Component {
     
 
     render() {
-        const {reservations} = this.props
-        console.log(reservation)
+    
+        
         return (
             <div>
                 History of Reservations
-                <p>{reservations.locationName}</p>
+           
               
             </div>
         )
