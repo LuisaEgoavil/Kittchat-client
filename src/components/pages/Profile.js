0066767442f1c1f99bd55e58ce2afe61f67ecb79
Luisa Egoavil <lupi@Luisas-Air.fritz.box>
2021-03-09 11:05:11 +0100

    commit

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -8,25 +8,53 @@ class Profile extends Component {
         reservations : []
     }
 
-    componentDidMount(){
-        //let reservationId = this.props.match.params.id
+    getAlltheData = () => {
         axios.get(`${config.API_URL}/api/profile`)
-            .then((response) =>{
-                console.log(response.data)
-                this.setState({
-                    locationName: response.data,
-                    time: response.data, 
-                    date: response.data,
-                    description: response.data,
-                    reservationName: response.data
-                })
+            .then((response) => {
+                const data = response.data
+                this.setState({reservations: data})
+                console.log('data has been received')
             })
             .catch(() => {
-                console.log('error while getting data')
             })
     }
+
+    componentDidMount = () => {
+        this.getAlltheData()
+    }
+
+    displayReservations = (reservations) => {
+        return reservations.map((reservation, index) => {
+            <div key={index}>
+                <p>Cafe Location <br/>{reservation.locationName}</p>
+                    <p>Name <br/>{reservation.reservationName}</p>
+                    <p>Date <br/>{reservation.date}</p>
+                    <p>Time <br/>{reservation.time}</p>
+                    <p>Special request <br/>{reservation.description}</p>
+            </div>
+        })
+    }
+    // componentDidMount(){
+    //     //let reservationId = this.props.match.params.id
+    //     axios.get(`${config.API_URL}/api/profile`)
+    //         .then((response) =>{
+    //             console.log(response.data)
+    //             this.setState({
+    //                 _id: _id,
+    //                 locationName: response.data,
+    //                 time: response.data, 
+    //                 date: response.data,
+    //                 description: response.data,
+    //                 reservationName: response.data
+    //             })
+    //         })
+    //         .catch(() => {
+    //             console.log('error while getting data')
+    //         })
+    // }
+
     render() {
-        const {reservation, locationName, time, date, description, reservationName} = this.state
+        //const {reservations} = this.state
         
         return (
             <div>
@@ -34,14 +62,15 @@ class Profile extends Component {
                 reservations.map((reservation, index) => {
                     return <div class="main-body" key={index}>
                     <h1>Profile</h1>
+                    {reservation._id}
                     {reservation.time}
                     {reservation.date}
                     {reservation.locationName}
                 </div>  
                 })
             } */}
+            {this.displayReservations(this.state.reservations)}
 
-            {reservation.time}
             </div>
             
         )
