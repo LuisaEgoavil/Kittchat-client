2c5bba4d42c63c663517390b076ce46c0e86b114
Luisa Egoavil <lupi@Luisas-Air.fritz.box>
2021-03-10 16:09:34 +0100

    commit

--- src/components/auth/LogIn.js
+++ src/components/auth/LogIn.js
@@ -20,20 +20,6 @@ class LogIn extends Component {
             </div>
             <button type="submit">Log In</button>
           </form>
-        <br/><br/><br/><br/>
-
-       <h1>Login as an admin</h1>
-          <form onSubmit={this.props.onLogInAdmin}>
-            <div className="form-grooup">
-              <label>Email</label>
-              <input name="email" type="email"/>
-            </div>
-            <div className="form-grooup">
-              <label>Password</label>
-              <input name="password" type="password" />
-            </div>
-            <button type="submit">Log In</button>
-          </form>
         </div>
       </div>
     )
