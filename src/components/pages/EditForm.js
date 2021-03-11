2eb863ab186a8ead79205dd65fe3aa72ac9610a4
Megumikawa <megumi19901016@gmail.com>
2021-03-11 07:30:23 +0100

    add another edit form

--- src/components/pages/EditForm.js
+++ src/components/pages/EditForm.js
@@ -29,7 +29,6 @@ class EditForm extends Component {
         description:this.state.reservation.description
       }
       this.setState({reservation:reser})
-
     }
 
     handleDesChange=(event)=>{
@@ -37,10 +36,26 @@ class EditForm extends Component {
       let clonedReservation=JSON.parse(JSON.stringify(this.state.reservation))
       clonedReservation.description=description;
       this.setState({reservation:clonedReservation})
-
     }
 
-   
+    handleDateChange=(event)=>{
+      let date=event.target.value;
+      let reser={
+        date:date,
+        description:this.state.reservation.description
+      }
+      this.setState({reservation:reser})
+    }
+
+    handleTimeChange=(event)=>{
+      let time=event.target.value;
+      let reser={
+        time:time,
+        description:this.state.reservation.description
+      }
+      this.setState({reservation:reser})
+    }
+
   //   handleDelete = (reservationId) => {
   //   axios.delete(`${config.API_URL}/api/bookinglist/${reservationId}`, {}, {withCredentials: true})
   //     .then(() => {
@@ -66,14 +81,31 @@ class EditForm extends Component {
         return (
       
           <form className="main-body" onSubmit={this.props.onEdit}>
-            
+              <div className="form-topic">
+                <label>Cafe Location</label><br></br>
+                <select name="locationName">
+                  <option value="Kittchat Schöneberg">Kittchat Schöneberg</option>
+                  <option value="Kittchat Charlottenburg">Kittchat Charlottenburg</option>
+                  <option value="Kittchat Kreuzberg">Kittchat Kreuzberg</option>
+                  <option value="Kittchat Neukölln">Kittchat Neukölln</option>
+                  <option value="Kittchat Prenzlauer Berg">Kittchat Prenzlauer Berg</option>
+                </select>
+              </div>
+              <div className="form-topic">
+                <label onChange={this.handleDateChange} value={reservation.date}>Date</label><br></br>
+                <input name="date" type="date" />
+              </div>
+              <div className="form-topic">
+                <label value={reservation.time}>Time</label><br></br>
+                <input onChange={this.handleTimeChange} name="time" type="time" />
+              </div>
               <div className="form-group">
-              <input hidden  value={reservation._id} name="id" type="text"/>
+              <input hidden value={reservation._id} name="id" type="text"/>
                 <label >Name</label>
                 <input onChange={this.handleNameChange} value={reservation.reservationName} name="reservationName" type="text"/>
               </div>
-            <div className="form-group">
-                <label>Description</label>
+              <div className="form-group">
+                <label>Special Request</label>
                 <input onChange={this.handleDesChange} value={reservation.description} name="description" type="text"/>
               </div>
               <button type="submit">Update</button>
