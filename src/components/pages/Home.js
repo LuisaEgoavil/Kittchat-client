44751c27b5ea0bbf8a8f8dfe2cc47d6e08494f5d
Megumikawa <megumi19901016@gmail.com>
2021-03-13 19:55:22 +0100

    feel change for heroku

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,7 +6,7 @@ import React, { Component } from 'react'
 class Home extends Component {
   render() {
     return (
-      <div>
+      <div className="home-height">
         <div style={{backgroundImage: `url(/images/bg2.jpg)`,backgroundSize: "cover",backgroundPosition:"center",backgroundRepeat:'no-repeat'}} className="main-body home-body">
           <div className="home-inside">
             <h1>Welcome to Kittchat</h1>
